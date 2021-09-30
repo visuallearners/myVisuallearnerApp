7b4ae59755cfcb843081ebebaaea30ac518540e8
visuallearners <fmiyare@gmail.com>
2021-09-30 21:55:51 +0300

    initial Author routes

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const expressLayouts = require('express-ejs-layouts')
 const port = 3000
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -29,5 +32,6 @@ mongoose.connect(process.env.URI , { useNewUrlParser : true, useUnifiedTopology 
 */
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || port)
